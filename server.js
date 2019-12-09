aafc9896595a0144d10b1b2507aa8d1f644e9333
Abderrahman <abderrahman@Abderrahmans-MacBook-Air.local>
2019-12-09 10:07:35 +0100

    Testing Routes

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const connectDB = require("./config/db.js");
 
 const app = express();
 
+// Database Connect
+connectDB();
+
+// Index Route
 app.get("/", (req, res) => res.send("API Running!"));
+
+// API Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/dashboard", require("./routes/api/dashboard"));
+app.use("/api/categories", require("./routes/api/categories"));
+app.use("/api/goals", require("./routes/api/goals"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Running on Port ${PORT}`));
